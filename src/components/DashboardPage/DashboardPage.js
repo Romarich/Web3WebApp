7e3d6dbd20822b0e823a485dc454d9b7c2b40dfb
Romaric HONOREZ <romaric.honorez@student.vinci.be>
2019-10-04 11:07:15 +0200

    commit le current state

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -2,14 +2,21 @@ import React from 'react'
 
 import Quote from '../Quote/Quote'
 import Title from '../Title/Title'
+import Footer from '../Footer/Footer'
 
-
+import './DashboardPage.css'
 
 function DashboardPage() {
+  var date = new Date();
+  var heure = date.getHours();
+  var minute = date.getMinutes();
+  var seconde = date.getSeconds();
+  var str = heure + ":" + minute + ":" + seconde;
   return (
     <div>
     <Title>Citations</Title>
     <Quote message="Never trust a computer you can't throw out a window." author="Steve Wozniak"></Quote>
+    <Footer date= {str}></Footer>
     </div>
   )
 }
