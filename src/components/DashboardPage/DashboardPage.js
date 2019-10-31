42f3ed29f451eb76920066fa40787bddb77de4bc
Honorez Romaric <romaric.honorez@student.vinci.be>
2019-10-31 13:47:22 +0100

    pour le merge

--- src/components/DashboardPage/DashboardPage.js
+++ src/components/DashboardPage/DashboardPage.js
@@ -2,14 +2,24 @@ import React from 'react'
 
 import Quote from '../Quote/Quote'
 import Title from '../Title/Title'
+import Footer from '../Footer/Footer'
 
+import './DashboardPage.css'
 
-
+//Juste chercher comment faire pour reprendre la date du navigateur
 function DashboardPage() {
+  var date = new Date();
+  var heure = date.getHours();
+  var minute = date.getMinutes();
+  var seconde = date.getSeconds();
+  var str = heure + ":" + minute + ":" + seconde;
   return (
     <div>
     <Title>Citations</Title>
     <Quote message="Never trust a computer you can't throw out a window." author="Steve Wozniak"></Quote>
+    <Quote message="C'est dans les moments les plus sombres qu'on voit le mieux les étoiles" author="Charles A. Beard"></Quote>
+    <Quote message="I've never met a strong person with an easy past" author="Atticus"></Quote>
+    <Footer date= {str}></Footer>
     </div>
   )
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Footer.css'
+
+function Footer(props) {
+    return (
+        <footer>
+            <h4>{props.date}</h4>
+        </footer>
+    )
+}
+
+export default Footer;
